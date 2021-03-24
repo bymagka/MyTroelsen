7864516adfe59ae36a149cf6999a45de246a04a4
bymagka <bymagka@bk.ru>
2021-03-24 22:49:39 +0300

    end chapter 26

---
+++ MyTroelsen/RenderingWithVisuals/CustomVisualFrameworkElement.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace RenderingWithVisuals
+{
+    public class CustomVisualFrameworkElement : FrameworkElement
+    {
+        VisualCollection theVisuals;
+
+        public CustomVisualFrameworkElement()
+        {
+            theVisuals = new VisualCollection(this) { AddRect(), AddCircle() };
+            this.MouseDown += CustomVisualFrameworkElement_MouseDown;
+        }
+
+        private void CustomVisualFrameworkElement_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Point t = e.GetPosition((UIElement)sender);
+
+            VisualTreeHelper.HitTest(this, null, new HitTestResultCallback(myCallback), new PointHitTestParameters(t));
+        }
+
+        private HitTestResultBehavior myCallback(HitTestResult result)
+        {
+            if(result.VisualHit.GetType() == typeof(DrawingVisual))
+            {
+                if(((DrawingVisual)result.VisualHit).Transform == null)
+                {
+                    ((DrawingVisual)result.VisualHit).Transform = new SkewTransform(20, 20);
+                }
+            }
+            else ((DrawingVisual)result.VisualHit).Transform = null;
+
+            return HitTestResultBehavior.Stop;
+        }
+
+        private Visual AddCircle()
+        {
+            DrawingVisual drawingVisual = new DrawingVisual();
+
+            using (DrawingContext dc = drawingVisual.RenderOpen())
+            {
+                dc.DrawEllipse(Brushes.Green, null, new Point(70, 90), 40, 50);
+            }
+
+            return drawingVisual;
+        }
+
+        private Visual AddRect()
+        {
+            DrawingVisual drawingVisual = new DrawingVisual();
+
+            using (DrawingContext dc = drawingVisual.RenderOpen())
+            {
+                dc.DrawRectangle(Brushes.Black, new Pen(Brushes.Yellow, 2), new Rect(new Point(100, 100), new Size(320, 80)));
+            }
+
+            return drawingVisual;
+        }
+
+        protected override int VisualChildrenCount => theVisuals.Count;
+
+        protected override Visual GetVisualChild(int index)
+        {
+            if (index < 0 || index >= theVisuals.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            return theVisuals[index];
+        }
+    }
+}
---
+++ MyTroelsen/RenderingWithVisuals/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RenderingWithVisuals
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            const int TextFontSize = 30;
+
+            FormattedText text = new FormattedText("Hello visual layer!",
+                new System.Globalization.CultureInfo("en-us"),
+                FlowDirection.LeftToRight,
+                new Typeface(this.FontFamily, FontStyles.Italic, FontWeights.DemiBold, FontStretches.UltraExpanded),
+                TextFontSize, Brushes.Green, null, VisualTreeHelper.GetDpi(this).PixelsPerDip);
+
+            DrawingVisual drawingVisual = new DrawingVisual();
+
+            using (DrawingContext drawingContext = drawingVisual.RenderOpen())
+            {
+                drawingContext.DrawRoundedRectangle(Brushes.Yellow, new Pen(Brushes.Green, 3), new Rect(5, 5, 450, 100), 20, 20);
+                drawingContext.DrawText(text, new Point(20, 20));
+            }
+
+            RenderTargetBitmap bmp = new RenderTargetBitmap(500, 100, 100, 90, PixelFormats.Pbgra32);
+
+            bmp.Render(drawingVisual);
+
+            myImage.Source = bmp;
+        }
+    }
+}
