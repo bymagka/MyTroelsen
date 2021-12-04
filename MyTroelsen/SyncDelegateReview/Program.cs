3149e6193e9d349abec2869bf06a8796759d64ac
bymagka <bymagka@bk.ru>
2021-12-04 21:07:15 +0300

    Concurrency delegates

---
+++ MyTroelsen/SyncDelegateReview/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Runtime.Remoting.Messaging;
+
+namespace SyncDelegateReview
+{
+    public delegate int BinaryOp(int x, int y);
+
+    class Program
+    {
+        static bool isDone = false;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main() invoked in thread {0}.", Thread.CurrentThread.ManagedThreadId);
+
+            BinaryOp op = new BinaryOp(Add);
+
+            IAsyncResult result = op.BeginInvoke(10, 10,new AsyncCallback(AddComplete), "Main thanks you for adding these  numbers");
+
+            while(!isDone)
+            {
+                Console.WriteLine("Doing more work in Main()!");
+                Thread.Sleep(1000);
+            }
+      
+            Console.ReadLine();
+
+        }
+
+
+        static int Add(int x,int y)
+        {
+            Console.WriteLine("Add() invoked on thread {0}.", Thread.CurrentThread.ManagedThreadId);
+
+            Thread.Sleep(5000);
+
+            return x + y;
+        }
+
+        static void AddComplete(IAsyncResult ar)
+        {
+            AsyncResult result = (AsyncResult)ar;
+
+            string message = (string)ar.AsyncState;
+
+            BinaryOp op = (BinaryOp)result.AsyncDelegate;
+
+            Console.WriteLine("10 + 10 is {0}.", op.EndInvoke(ar));
+
+            Console.WriteLine(message);
+
+            Console.WriteLine("AddComplete() invoked on thread {0}.", Thread.CurrentThread.ManagedThreadId);
+
+            isDone = true;
+
+        }
+    }
+}
---
+++ MyTroelsen/TechRace/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+public class Test
+{
+	public static void Main()
+	{
+        //Console.OutputEncoding = Encoding.UTF8;
+        //Console.InputEncoding = Encoding.UTF8;
+        string response = "something: ";
+		string line;
+		while ((line = Console.ReadLine()) != null && line != "")
+		{
+			response += line; // get the line
+		}
+
+		response = ConvertString(response);
+
+		//here is your code
+		Console.WriteLine(response);// return the string as a response
+		Console.ReadLine();
+	}
+
+    private static string ConvertString(string response)
+    {
+		response = response.Replace("something: ", "");
+
+		StringBuilder sb = new StringBuilder();
+		
+
+		char[] alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+
+		int shift = int.Parse(response[response.Length-1].ToString());
+
+		response = response.Substring(0, response.Length - 1);
+
+		foreach(var item in response)
+        {
+			if (!alphabet.Contains(char.ToLower(item)))
+            {
+				sb.Append(item);
+				continue;
+			}
+	
+
+			int index = Array.IndexOf(alphabet, char.ToLower(item));
+
+			if ((index - shift) >= alphabet.Length)
+				index = (index - shift) - alphabet.Length - 1;
+			else if ((index - shift) < 0)
+				index = alphabet.Length - (shift - index);
+			else
+				index = index - shift;
+
+			sb.Append(char.IsUpper(item) ? char.ToUpper(alphabet[index]) : alphabet[index]);
+        }
+
+		return sb.ToString();
+	
+		
+    }
+}
