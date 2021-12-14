a4d2a974366481903aba0ab30145617ead035bba
bymagka <bymagka@bk.ru>
2021-12-14 13:12:24 +0300

    async

---
+++ MyTroelsen/FunWithCSharpAsync/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace FunWithCSharpAsync
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Fun with async");
+
+            List<int> l = default;
+
+            Console.WriteLine(DoWork());
+            Console.WriteLine("Completed");
+
+            Console.ReadLine();
+        }
+
+        static string DoWork()
+        {
+            Thread.Sleep(5_000);
+            return "Done with work";
+        }
+    }
+}
---
+++ MyTroelsen/PLINQDataWithCancellation/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace PLINQDataWithCancellation
+{
+    class Program
+    {
+        static CancellationTokenSource cancelToken = new CancellationTokenSource();
+
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Any key");
+
+                Console.ReadKey();
+
+                Console.WriteLine("Processing");
+
+                Task.Factory.StartNew(() => ProcessIntData());
+
+                Console.WriteLine("Enter q to quit");
+
+                string answer = Console.ReadLine();
+
+                if (answer.Equals("Q", StringComparison.OrdinalIgnoreCase))
+                {
+                    cancelToken.Cancel();
+                    break;
+                }
+
+            } while (true);
+
+           
+
+
+            Console.ReadLine();
+        }
+
+
+        static void ProcessIntData()
+        {
+            int[] source = Enumerable.Range(1, 100_000_000).ToArray();
+
+            int[] modThreeIsZero = null;
+
+
+            try
+            {
+                modThreeIsZero = (from num in source.AsParallel().WithCancellation(cancelToken.Token)
+                                  where num % 3 == 0
+                                  orderby num descending
+                                  select num).ToArray();
+
+                Console.WriteLine();
+
+                Console.WriteLine($"Found {modThreeIsZero.Count()} numbers that match query");
+
+            }
+            catch (OperationCanceledException ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+
+           
+
+            
+        }
+    }
+}
